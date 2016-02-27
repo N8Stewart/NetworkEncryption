e83a4f619f9d4b062492c94b7e9d0c27f7eb0095
N8Stewart <Nate.Stewart12@gmail.com>
2016-02-27 18:25:13 -0500

    Initial commit. Created initial network communication between computers.

---
+++ constants.py
@@ -0,0 +1,14 @@
+#!/usr/local/python-3.2.3/bin/python3.2
+
+# Nate Stewart
+# 02/27/16
+# Network Encryption constants file
+
+# Symbolic name meaning all available interfaces
+HOST = '' 
+# Port number that server will be listening on
+DEFAULT_PORT = 5989
+# Name of the shell when no connection is established
+DEFAULT_SHELL = "NEC"
+# Constant buffer size to use when reading and writing to buffer
+BUFFER_SIZE = 512
